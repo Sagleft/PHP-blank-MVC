bbcaae08f4e70a56b1aae4bc99268290c598f3e4
Sagleft <sagleft@inbox.ru>
2019-09-10 02:13:27 +0400

    optimize handler & fix head.tmpl

--- controller/src/Handler.php
+++ controller/src/Handler.php
@@ -12,13 +12,13 @@
 		
 		public function __construct() {
 			$this->enviro  = new \App\Model\Environment();
-			$this->db      = new \App\Model\DataBase();
+			//$this->db      = new \App\Model\DataBase();
 			$this->logic   = new \App\Controller\Logic();
 			$this->user    = new \App\Controller\User();
 			$this->renderT = new \App\Controller\Render([]);
 			
-			$this->logic->setdb($this->db);
-			$this->user->setdb($this->db);
+			//$this->logic->setdb($this->db);
+			//$this->user->setdb($this->db);
 			$this->logic->setUser($this->user);
 		}
 		
