704b940f33b527f1484ae429343d0fc97b7f7e1e
Sagleft <sagleft@inbox.ru>
2020-05-01 22:44:11 +0300

    update install script
    add make dir for logs

--- install.sh
+++ install.sh
@@ -3,5 +3,8 @@ chmod 777 view/cache
 cp example.env .env
 cp composer.example.json composer.json
 composer update
-cd controller/public_html
+mkdir logs
+cd logs
+touch access.log error.log
+cd ../controller/public_html
 cp example.htaccess .htaccess
