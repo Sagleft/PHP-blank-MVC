6cb449798d4b33211bf30c5681577c36a0f45429
Sagleft <sagleft@inbox.ru>
2020-05-01 22:43:23 +0300

    update Handler
    added method isDBEnabled

--- controller/src/Handler.php
+++ controller/src/Handler.php
@@ -5,23 +5,34 @@
 		public $logic = null;
 		public $user  = null;
 		public $renderT = null;
-		public $last_error = "";
-		
-		private $db      = null;
-		private $enviro  = null;
-		
+		public $last_error = '';
+
+		protected $db      = null;
+		protected $enviro  = null;
+		protected $db_enabled = false;
+
 		public function __construct() {
 			$this->enviro  = new \App\Model\Environment();
-			//$this->db      = new \App\Model\DataBase();
+			$this->db_enabled = getenv('enable_db') == '1';
+			if($this->isDBEnabled()) {
+				$this->db = new \App\Model\DataBase();
+			}
+
 			$this->logic   = new \App\Controller\Logic();
 			$this->user    = new \App\Controller\User();
 			$this->renderT = new \App\Controller\Render([]);
-			
-			//$this->logic->setdb($this->db);
-			//$this->user->setdb($this->db);
+
+			if(if($this->isDBEnabled())) {
+				$this->logic->setdb($this->db);
+				$this->user->setdb($this->db);
+			}
 			$this->logic->setUser($this->user);
 		}
-		
+
+		function isDBEnabled(): bool {
+			return $this->db_enabled;
+		}
+
 		public function render($data = []) {
 			$this->renderT = new \App\Controller\Render($data);
 			$this->renderT->twigRender();
