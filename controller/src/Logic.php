9e0fda050dceddeac04d9dcaae968a090da7bc7a
Sagleft <sagleft@inbox.ru>
2020-05-01 22:43:42 +0300

    change method types from private to protected

--- controller/src/Logic.php
+++ controller/src/Logic.php
@@ -1,20 +1,19 @@
 <?php
 	namespace App\Controller;
-	
+
 	class Logic {
 		public $user = null;
-		private $db  = null;
-		
+		protected $db  = null;
+
 		public function __construct() {
 			//
 		}
-		
+
 		public function setdb($db) {
 			$this->db = &$db;
 		}
-		
+
 		public function setUser($user): void {
 			$this->user = &$user;
 		}
 	}
-	
